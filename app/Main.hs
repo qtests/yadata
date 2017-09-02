965140fe5f273310e58543ec8ef4143e2f11b175
qtests <qtests@outlook.com>
2017-09-02 21:34:31 +0200

    new file:   .gitignore 	modified:   app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Lib
 
 
 -- http://hackage.haskell.org/package/wreq-0.5.1.0/docs/Network-Wreq-Session.html
+-- https://stackoverflow.com/questions/44044263/yahoo-finance-historical-data-downloader-url-is-not-working
 
 main :: IO ()
 main = do
