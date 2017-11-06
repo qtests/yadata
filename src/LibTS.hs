3c661ce23a3dc94d66bbf5bfe8e9557019f4be6f
qtests <qtests@outlook.com>
2017-11-06 20:23:42 +0100

    modified:   src/LibTS.hs

--- src/LibTS.hs
+++ src/LibTS.hs
@@ -8,6 +8,7 @@ module LibTS
     getDateTimeInterval,
     isAWorkingDay,
     alignTS,
+    alignTSIndex,
     backFillTS
 ) where
 
@@ -29,41 +30,50 @@ isAWorkingDay x =
 -- https://two-wrongs.com/haskell-time-library-tutorial
 
 getDateTimeInterval' :: Num a => [(UTCTime, a)] -> [UTCTime]
-getDateTimeInterval' tseries = []
+getDateTimeInterval' tseries = 
+    fmap (\x -> addUTCTime (24*60*60*( fromRational x)) mint) [0 .. timediff]
+    where
+        (dates, _) = unzip tseries
+        mint = minimum dates
+        maxt = maximum dates
+        timediff = (toRational (diffUTCTime maxt mint)) / (60*60*24)
 
 
 getDateTimeInterval :: (Ord a, Num a) => Either String [(UTCTime, a)] -> Either String [UTCTime]
 getDateTimeInterval tseries = do
-    let mint = fmap (fst . minimum) tseries
-    let maxt = fmap (fst . maximum) tseries
-    let timediff = fmap (\x -> (toRational x) / (60*60*24) ) (diffUTCTime <$> maxt <*> mint)
-    case mint of
-        Left _ -> return []
-        Right mt -> do
-            nofDays <- timediff
-            return $ fmap (\x -> addUTCTime (24*60*60*( fromRational x)) mt) [0 .. (toRational nofDays)]
+    tseries' <- tseries
+    return $ getDateTimeInterval' tseries'
 
 
 -- https://downloads.haskell.org/~ghc/6.12.2/docs/html/libraries/containers-0.3.0.0/Data-Map.html
 alignTS' :: Num a => [UTCTime] -> [(UTCTime, a)] -> [(UTCTime, Maybe a)]
-alignTS' [] ts = map (second (Just))  ts  -- Add "getDateTimeInterval"
+alignTS' [] ts = alignTS' ( filter isAWorkingDay $ getDateTimeInterval' ts) ts  -- Add "getDateTimeInterval"
 alignTS' _ [] = []
 alignTS' idx ts = zip idx' combinedValues
      where   tvMap = foldl (\mm (key, value) -> Map.insert key value mm) Map.empty ts
              idx' = sort idx
              combinedValues = map (\v -> Map.lookup v tvMap) idx'                          
 
-alignTS :: Num a => Either String [UTCTime] -> Either String [(UTCTime, a)] -> Either String [(UTCTime, Maybe a)]                    
-alignTS idx ts = do 
+
+alignTSIndex :: Num a => Either String [UTCTime] -> Either String [(UTCTime, a)] -> Either String [(UTCTime, Maybe a)]                    
+alignTSIndex idx ts = do 
     ind <- idx
     dta <- ts
     return $ alignTS' ind dta
+
+
+alignTS :: Num a => Either String [(UTCTime, a)] -> Either String [(UTCTime, Maybe a)]                    
+alignTS ts = do 
+    dta <- ts
+    return $ alignTS' [] dta
+
 
 backFillTS' :: Num a => [Maybe a] -> [Maybe a]
 backFillTS' [] = []
 backFillTS' (x:[]) = [x]
 backFillTS' (x:y:[]) = if (isNothing y) then (x:x:[]) else  (x:y:[])
 backFillTS' (x:y:rest) = if (isJust x && isNothing y) then backFillTS' (x:x:rest) else  (x:( backFillTS' (y:rest) ))
+
 
 backFillTS :: Num a => Either String [(UTCTime, Maybe a)] -> Either String [(UTCTime, Maybe a)] 
 backFillTS ts = do 
@@ -72,8 +82,9 @@ backFillTS ts = do
     return $ zip tsIndex (backFillTS' values)
     
 
+-- allignAndBackfill ??
+
 -- ts <- priceTimeSeries "IBM"
 -- let ts1 = fmap (take 20) ts
--- let tsIndex = fmap (filter isAWorkingDay) $ getDateTimeInterval ts1
--- let tsa = alignTS tsIndex ts1
+-- let tsa = alignTS ts1
 -- let tsb = backFillTS tsa
