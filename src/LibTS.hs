33dee3dd0d204fc5e2d44e60ed5d725fc2eaf43a
qtests <qtests@outlook.com>
2017-11-10 21:27:17 +0100

    new file:   labas.csv 	modified:   src/LibTS.hs

--- src/LibTS.hs
+++ src/LibTS.hs
@@ -10,16 +10,24 @@ module LibTS
     alignTS,
     alignTSIndex,
     backFillTS,
-    alignAndBackfillTS
+    alignAndBackfillTS,
+    TS(..),
+    createTSRaw,
+    createTSEither,
+    writeFileTS,
+    readFileTS
 ) where
 
 import Data.Time
 import Data.Time.Calendar.WeekDate
 import qualified Data.Map as Map
-import Control.Arrow (second)
 import Data.List
 import Data.Maybe
+import Data.Either
+import Text.CSV
+import Control.Arrow (second)
 
+import LibCSV
 -- ###########################################################################
 
 isAWorkingDay::UTCTime -> Bool
@@ -30,17 +38,23 @@ isAWorkingDay x =
 
 -- https://two-wrongs.com/haskell-time-library-tutorial
 
-getDateTimeIntervalTS' :: Num a => [(UTCTime, a)] -> [UTCTime]
-getDateTimeIntervalTS' tseries = 
+getDateTimeInterval :: [UTCTime] -> [UTCTime]
+getDateTimeInterval timeIndex = 
     fmap (\x -> addUTCTime (24*60*60*( fromRational x)) mint) [0 .. interval]
     where
-        (dates, _) = unzip tseries
-        mint = minimum dates
-        maxt = maximum dates
+        mint = minimum timeIndex
+        maxt = maximum timeIndex
         interval = (toRational (diffUTCTime maxt mint)) / (60*60*24)
 
 
-getDateTimeIntervalTS :: (Ord a, Num a) => Either String [(UTCTime, a)] -> Either String [UTCTime]
+getDateTimeIntervalTS' :: Num a => [(UTCTime, a)] -> [UTCTime]
+getDateTimeIntervalTS' tseries = 
+    getDateTimeInterval dates
+    where
+        (dates, _) = unzip tseries
+
+
+getDateTimeIntervalTS :: Num a => Either String [(UTCTime, a)] -> Either String [UTCTime]
 getDateTimeIntervalTS tseries = do
     tseries' <- tseries
     return $ getDateTimeIntervalTS' tseries'
@@ -83,10 +97,9 @@ backFillTS ts = do
     return $ zip tsIndex (backFillTS' values)
     
 
-alignAndBackfillTS :: (Eq a, Num a) => Either String [(UTCTime, a)] -> Either String [(UTCTime, a)]
+alignAndBackfillTS :: (Eq a, Num a) => [(UTCTime, a)] -> Either String [(UTCTime, a)]
 alignAndBackfillTS ts = do
-    dta <- ts
-    let tsa =  alignTS' [] dta 
+    let tsa =  alignTS' [] ts 
     let (tsIndex, values) = unzip tsa
     let valuesB = backFillTS' values
     let values' = if any (== Nothing) valuesB 
@@ -95,9 +108,52 @@ alignAndBackfillTS ts = do
         then Left "No data!"
         else return $ zip tsIndex ( catMaybes values' )
 
+-- TS -------------------------------------------------------------------------------------------
+-- **********************************************************************************************
+
+data TS a = TS [UTCTime] [a]
+
+createTSRaw :: (Eq a, Num a) => [UTCTime] -> [a] -> TS a
+createTSRaw times values = TS abtimes abvalues
+    where 
+        ab = alignAndBackfillTS (zip times values)
+        (abtimes, abvalues) = if (isLeft ab) then ([], []) else unzip (concat $ rights [ab])
+
+        
+createTSEither :: (Eq a, Num a) => Either String [(UTCTime, a)]  -> TS a
+createTSEither ts = TS abtimes abvalues
+    where
+        ts1 = if (isLeft ts) then [] else (concat $ rights [ts])
+        ab = alignAndBackfillTS ts1 
+        (abtimes, abvalues) = if (isLeft ab) then ([], []) else unzip (concat $ rights [ab])
+
+
+instance Show a => Show (TS a) where
+    show (TS times values) = mconcat rows
+      where rows = zipWith (\x y -> mconcat [show x," | ",show y,"\n"] ) times values
+
+
+writeFileTS :: (Show a) => FilePath -> TS a -> IO ()
+writeFileTS path (TS times values) =
+    writeFile path tsString
+    where
+        -- formatTime defaultTimeLocale "%F %T (%Z)" x
+        tsString = mconcat $ ["Date,Value\n"] ++ zipWith (\x y -> mconcat [show x,",",show y,"\n"] ) times values
+
+
+readFileTS :: FilePath -> IO (TS Double)
+readFileTS path = do
+    let tstext = readFile path
+    txt <- tstext
+    let ptxt = parseCSV path txt
+    let date =  either (\_-> []) (\x-> fmap (read2UTCTime "%Y-%m-%d %H:%M:%S %Z") x) (getColumnInCSV ptxt "Date")
+    let value = either (\_-> []) (\x-> fmap read2Double x) (getColumnInCSV ptxt "Value")
+    return $ TS date value
+ 
+
 -- XTS ?
 
 -- ts <- priceTimeSeries "IBM"
 -- let ts1 = fmap (take 20) ts
--- let tsa = alignTS ts1
--- let tsb = backFillTS tsa
+-- let tx = createTSEither ts1
+-- print tx
